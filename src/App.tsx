2ffdb0f9a5e1e2142d6dcdd6c4d634492c958c5a
omarlagargola <79851501+omarlagargola@users.noreply.github.com>
2021-04-03 19:46:14 +0200

    cognigy chat

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import ChatBot from './components/ChatBot';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <ChatBot></ChatBot>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ChatBot.jsx
@@ -0,0 +1,140 @@
+import React, { useState, useEffect, useRef } from "react";
+import makeStyles from "@material-ui/core/styles/makeStyles";
+import {
+  TextField,
+  List,
+  ListItem,
+  Chip,
+  Typography,
+  Box,
+  Paper,
+} from "@material-ui/core";
+import { SocketClient } from "@cognigy/socket-client";
+
+const useStyles = makeStyles((theme) => ({
+  chat: {
+    height: "600px",
+    width: "300px",
+    backgroundColor: "azure",
+  },
+  chatList: {
+    height: "100%",
+    overflowY: "auto",
+    padding: 0,
+  },
+  innerMessage: {
+    whiteSpace: "break-spaces",
+    padding: "10px",
+  },
+  message: {
+    height: "auto",
+    whiteSpace: "break-spaces",
+    wordBreak: "break-word",
+  },
+  botMessage: {
+    float: "left",
+    borderTopLeftRadius: 0,
+  },
+  userMessage: {
+    float: "right",
+    borderTopRightRadius: 0,
+  },
+  chatListItem: {
+    display: "inline-block",
+  },
+  input: {
+    background: "white",
+    marginTop: "20px",
+  },
+}));
+
+const ENDPOINT_URL = "https://endpoint-demo.cognigy.ai";
+const URL_TOKEN = "ce5c41bdbd3cc71fbb81b0f192e46c9b1f306988cc03d9bc5a348ad96d249aba";
+
+let client = new SocketClient(ENDPOINT_URL, URL_TOKEN, {
+  // if you use node, internet explorer or safari, you need to enforce websockets
+  forceWebsockets: true,
+});
+
+function ChatBot() {
+  const css = useStyles();
+
+  const enterKeyCode = 13;
+  const [userMessage, setUserMessage] = useState("");
+  const [messagesList, setMessagesList] = useState([{id:0, message:"I am chatbot", isBot:true}]);
+
+  const chatRef = useRef();
+  const inputRef = useRef();
+
+  const handleMessage = (e) => {
+    setUserMessage(e.target.value);
+  };
+
+  const handleMessageSent = async (e) => {
+    if (e.keyCode === enterKeyCode && userMessage.length !== 0) {
+      setUserMessage("");
+      setMessagesList((prevState) => [
+        ...prevState,
+        { id: prevState[prevState.length-1].id+1, message: userMessage, isBot: false },
+      ]);
+      client.sendMessage(userMessage);
+    }
+  };
+
+  useEffect(() => {
+    client.connect();
+    client.on("output", (output) => {
+      console.log(output);
+      if(output.data !== null){
+        setMessagesList((prevState) => [
+          ...prevState,
+          { id: prevState[prevState.length-1].id+1 , message: output.text, isBot: true },
+        ]);
+      }
+    });
+  }, []);
+
+  useEffect(() => {
+    inputRef.current.children[0].focus()
+    chatRef.current.scrollIntoView({ behavior: "smooth", block: "end"});
+  }, [messagesList]);
+
+  return(
+    <>
+      <Paper elevation={3} className={css.chat}>
+        <List className={css.chatList}>
+          <Box ref={chatRef}>
+            {messagesList.map(({ id, message, isBot }) => (
+              <ListItem className={css.chatListItem} key={id}>
+                <Chip
+                  label={
+                    <Typography variant="body2" className={css.innerMessage}>
+                      {message}
+                    </Typography>
+                  }
+                  color={isBot ? "secondary" : "primary"}
+                  className={`${css.message} ${
+                    isBot ? css.botMessage : css.userMessage
+                  }`}
+                />
+              </ListItem>
+            ))}
+          </Box>
+        </List>
+        <TextField
+          className={css.input}
+          autoFocus={true}
+          id="outlined-basic"
+          label="Please write your message"
+          variant="outlined"
+          ref={inputRef}
+          onChange={handleMessage}
+          onKeyDown={handleMessageSent}
+          value={userMessage}
+        />
+      </Paper>
+    </>
+  );
+}
+
+export default ChatBot;
